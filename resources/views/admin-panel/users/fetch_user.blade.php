411a1b9b0629f16b36c6be854cbe01da5bebec13
Tauseef Ullah <43759125+Tauseefislamian232@users.noreply.github.com>
2022-09-26 17:55:04 +0500

    parcel large --saad

--- resources/views/admin-panel/users/fetch_user.blade.php
+++ resources/views/admin-panel/users/fetch_user.blade.php
@@ -123,6 +123,9 @@
                                 </p>
                                 <hr><br><br>
                                 @if (isset($data))
+
+                                <div class="table-rep-plugin">
+                                    <div class="table-responsive mb-0" data-pattern="priority-columns">
                                     <table id="datatable-buttons"
                                         class="table table-bordered dt-responsive nowrap w-100 table-sm text-center">
 
@@ -166,7 +169,7 @@
                                                             @endif
                                                         </td> --}}
                                                         <td>
-                                                            
+
                                                             {{-- <a href="{{ url('edit-country/' . $item->id) }}"
                                                                 class="btn btn-outline-warning btn-sm edit" title="Edit">
                                                                 <i class="fas fa-pencil-alt"></i>
@@ -176,8 +179,8 @@
                                                                 onclick="return confirm('Are you sure to delete Record?')">
                                                                 <i class="fas fa-trash-alt"></i>
                                                             </a>
-                                                            
-                                                        </td>     
+
+                                                        </td>
                                                     </tr>
                                                 @endforeach
                                             @else
@@ -190,6 +193,8 @@
 
                                         </tbody>
                                     </table>
+                                </div>
+                            </div>
                                 @endif
                             </div>
 
